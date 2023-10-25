e7b5f8b26d3e7d3e090e75aa46d1d50af8c46966
KeshalHetti <kaneeshakeshal@gmail.com>
2023-10-25 22:31:05 +1100

    Added Styling to the pages

--- src/Components/lectureanswer.jsx
+++ src/Components/lectureanswer.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import Box from '@mui/material/Box';
+import BannerBackground from "../Assets/home-banner-background.png";
+import { Box, Card, CardContent, Typography, Button } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import Button from '@mui/material/Button';
 
 export default function QuestionPage() {
   const [name, setName] = useState('');
@@ -32,61 +32,82 @@ export default function QuestionPage() {
   };
 
   return (
-    <div>
-      <h1>Ask a Question</h1>
-      <Box
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="standard-basic"
-          label="Name of the student"
-          variant="standard"
-          value={name}
-          onChange={handleNameChange}
-        />
-      </Box>
+    <div className="home-container">
 
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Subject</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={subject}
-            onChange={handleSubjectChange}
-          >
-            <MenuItem value="English">English</MenuItem>
-            <MenuItem value="Maths">Maths</MenuItem>
-            <MenuItem value="Science">Science</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            BrightBoost Lecture Profile
+          </h1>
+        </div>
+      </div>
 
-      <Box
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="outlined-basic"
-          label="Type your answer here"
-          variant="outlined"
-          value={question}
-          onChange={handleQuestionChange}
-        />
-      </Box>
+      <div className='lecturer-boxcontainer'>
+        <Card className="lecturer-card">
+          <CardContent>
+            <Typography gutterBottom variant='h4' component='div'>
+              Ask a Question
+            </Typography>
+            <Box
+              component="form"
+              sx={{
+                '& > :not(style)': { m: 1, width: '25ch' },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="standard-basic"
+                label="Name of the student"
+                variant="standard"
+                value={name}
+                onChange={handleNameChange}
+              />
+            </Box>
 
-      <Button variant="contained" onClick={handleQuestionSubmit}>
-        Submit Answer
-      </Button>
-    </div>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl sx={{
+                '& > :not(style)': { m: 1, width: '70ch' },
+              }}>
+                <InputLabel id="demo-simple-select-label">Subject</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={subject}
+                  onChange={handleSubjectChange}
+                >
+                  <MenuItem value="English">English</MenuItem>
+                  <MenuItem value="Maths">Maths</MenuItem>
+                  <MenuItem value="Science">Science</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+
+            <Box
+              component="form"
+              sx={{
+                '& > :not(style)': { m: 1, width: '70ch' },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="outlined-basic"
+                label="Type your answer here"
+                variant="outlined"
+                value={question}
+                onChange={handleQuestionChange}
+              />
+            </Box>
+            <Button variant="contained" onClick={handleQuestionSubmit}>
+              Submit Answer
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </div >
   );
 }
