351675520634a4c6a86d6468b6257cf2d637802f
KeshalHetti <kaneeshakeshal@gmail.com>
2023-10-27 17:22:54 +1100

    Changed the view of the user profile

--- src/Components/studentprofile.jsx
+++ src/Components/studentprofile.jsx
@@ -9,7 +9,6 @@ import Avatar from '@mui/material/Avatar';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
@@ -94,10 +93,19 @@ function SimpleDialog(props) {
               <PersonIcon />
             </Avatar>
           </ListItemAvatar>
-          <ListItemText
-            primary={user.name}
-            secondary={`Email: ${user.email}, Age: ${user.age}, Gender: ${user.gender}, Phone: ${user.contact}`}
-          />
+          <ListItemText primary={user.name} secondary="User Information" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Email: ${user.email}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Age: ${user.age}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Gender: ${user.gender}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Phone: ${user.contact}`} />
         </ListItem>
       </List>
     </Dialog>
