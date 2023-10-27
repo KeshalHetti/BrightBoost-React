80363b251f8e53ece7a4a483b6d28dd1a4006650
KeshalHetti <kaneeshakeshal@gmail.com>
2023-10-27 17:21:30 +1100

    Added the website theme to the page

--- src/Components/stimetable.jsx
+++ src/Components/stimetable.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { db } from '../config/firebase'; 
+import { db } from '../config/firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,6 +9,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import BannerBackground from "../Assets/home-banner-background.png";
 
 export default function StudentSchedule() {
   const [data, setData] = useState([]);
@@ -29,33 +30,46 @@ export default function StudentSchedule() {
   }, []);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Timetable</TableCell>
-            <TableCell align="right">Monday</TableCell>
-            <TableCell align="right">Tuesday</TableCell>
-            <TableCell align="right">Wednesday</TableCell>
-            <TableCell align="right">Thursday</TableCell>
-            <TableCell align="right">Friday</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row, rowIndex) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              {['monday', 'tuesday', 'wednesday', 'thursday', 'friday'].map((day, dayIndex) => (
-                <TableCell align="right" key={dayIndex}>
-                  {row[day]}
-                </TableCell>
-              ))}
+    <div className="home-container">
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            BrightBoost - Timetabe
+          </h1>
+        </div>
+      </div>
+
+      <TableContainer className='timetable' component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Timetable</TableCell>
+              <TableCell align="right">Monday</TableCell>
+              <TableCell align="right">Tuesday</TableCell>
+              <TableCell align="right">Wednesday</TableCell>
+              <TableCell align="right">Thursday</TableCell>
+              <TableCell align="right">Friday</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {data.map((row, rowIndex) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                {['monday', 'tuesday', 'wednesday', 'thursday', 'friday'].map((day, dayIndex) => (
+                  <TableCell align="right" key={dayIndex}>
+                    {row[day]}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
