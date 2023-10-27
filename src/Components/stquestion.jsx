eb9ab9c9bba9fa9e203ddcc9740297ae16f9d670
KeshalHetti <kaneeshakeshal@gmail.com>
2023-10-27 17:22:19 +1100

    Added a pop to verification for the questions

--- src/Components/stquestion.jsx
+++ src/Components/stquestion.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { auth, db } from '../config/firebase'; // Ensure the path to your firebase configuration is correct
+import { auth, db } from '../config/firebase';
 import { collection, addDoc, getDoc, doc, where, query, getDocs } from 'firebase/firestore';
 import BannerBackground from "../Assets/home-banner-background.png";
-import { Box, Card, CardContent, Typography, Button } from '@mui/material';
+import { Box, Card, CardContent, Typography, Button, Snackbar, Alert } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -15,17 +15,17 @@ export default function QuestionPage() {
   const [isStudent, setIsStudent] = useState(false);
   const [userName, setUserName] = useState('');
   const [answers, setAnswers] = useState([]);
-
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
   useEffect(() => {
     const fetchAnswers = async () => {
-        if (!auth.currentUser) {
-            console.error("User not logged in.");
-            return;
-        }
-        const userEmail = auth.currentUser.email;
-        const qnaSnapshot = await getDocs(query(collection(db, 'qna'), where('emailasked', '==', userEmail)));
-        setAnswers(qnaSnapshot.docs.map(doc => doc.data()));
+      if (!auth.currentUser) {
+        console.error("User not logged in.");
+        return;
+      }
+      const userEmail = auth.currentUser.email;
+      const qnaSnapshot = await getDocs(query(collection(db, 'qna'), where('emailasked', '==', userEmail)));
+      setAnswers(qnaSnapshot.docs.map(doc => doc.data()));
     }
 
     fetchAnswers();
@@ -33,63 +33,62 @@ export default function QuestionPage() {
 
   const handleQuestionSubmit = async () => {
     if (!auth.currentUser) {
-        console.error("User not logged in.");
-        return;
+      console.error("User not logged in.");
+      return;
     }
 
     try {
-        const userEmail = auth.currentUser.email;
-        const userSnapshot = await getDocs(query(collection(db, 'users'), where('email', '==', userEmail)));
+      const userEmail = auth.currentUser.email;
+      const userSnapshot = await getDocs(query(collection(db, 'users'), where('email', '==', userEmail)));
 
-        if (userSnapshot.empty) {
-            console.error(`No user data found for email: ${userEmail}`);
-            return;
-        }
+      if (userSnapshot.empty) {
+        console.error(`No user data found for email: ${userEmail}`);
+        return;
+      }
 
-        const userDoc = userSnapshot.docs[0];
-        const userData = userDoc.data();
+      const userDoc = userSnapshot.docs[0];
+      const userData = userDoc.data();
 
-        if (userData.role !== 'student') {
-            console.error('Only students can submit questions.');
-            return;
-        }
+      if (userData.role !== 'student') {
+        console.error('Only students can submit questions.');
+        return;
+      }
 
-        setUserName(userEmail);
+      setUserName(userEmail);
 
-        const timestampAsked = Date.now();
-        await addDoc(collection(db, 'qna'), {
-            emailasked: userEmail,
-            question: question,
-            timestampasked: timestampAsked,
-            timestampreplied: null,
-            timetaken: 0,
-            subject: subject
-        });
+      const timestampAsked = Date.now();
+      await addDoc(collection(db, 'qna'), {
+        emailasked: userEmail,
+        question: question,
+        timestampasked: timestampAsked,
+        timestampreplied: null,
+        timetaken: 0,
+        subject: subject
+      });
+      setSnackbarOpen(true); // Open the Snackbar
     } catch (error) {
-        console.error("Error fetching user role:", error);
+      console.error("Error fetching user role:", error);
     }
-};
+  };
 
-
-
-return (
-  <div className="home-container">
+  return (
+    <div className="home-container">
       <div className="home-banner-container">
-          <div className="home-bannerImage-container">
-              <img src={BannerBackground} alt="" />
-          </div>
-          <div className="home-text-section">
-              <h1 className="primary-heading">BrightBoost Lecture Profile</h1>
-          </div>
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">BrightBoost Lecture Profile</h1>
+        </div>
       </div>
       <div className='lecturer-boxcontainer'>
-          <Card className="lecturer-card">
-              <CardContent>
-                  <Typography gutterBottom variant='h4' component='div'>
-                      Ask a question
-                  </Typography>
+        <Card className="lecturer-card">
+          <CardContent>
+            <Typography gutterBottom variant='h4' component='div'>
+              Ask a question
+            </Typography>
 
-                  <Box sx={{
+            <Box sx={{
               '& > :not(style)': { m: 1, width: '70ch' },
             }}>
               <FormControl fullWidth>
@@ -127,24 +126,35 @@ return (
             <Button variant="contained" onClick={handleQuestionSubmit}>
               Submit Question
             </Button>
-              </CardContent>
+          </CardContent>
+        </Card>
+
+        {answers.map((answerData, index) => (
+          <Card className="lecturer-answer" key={index}>
+            <CardContent>
+              <Typography variant="h6">
+                Question: {answerData.question}
+              </Typography>
+              <Typography variant="subtitle1">
+                Answer: {answerData.answer || 'Awaiting answer...'}
+              </Typography>
+              <Typography variant="subtitle2">
+                Answered by: {answerData.emailanswered || 'Not answered yet'}
+              </Typography>
+            </CardContent>
           </Card>
-          {answers.map((answerData, index) => (
-          <Card key={index}>
-              <CardContent>
-                  <Typography variant="h6">
-                      Question: {answerData.question}
-                  </Typography>
-                  <Typography variant="subtitle1">
-                      Answer: {answerData.answer || 'Awaiting answer...'}
-                  </Typography>
-                  <Typography variant="subtitle2">
-                      Answered by: {answerData.emailanswered || 'Not answered yet'}
-                  </Typography>
-              </CardContent>
-          </Card>
-      ))}
+        ))}
       </div>
-  </div>
-);
+
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={() => setSnackbarOpen(false)}
+      >
+        <Alert onClose={() => setSnackbarOpen(false)} severity="success">
+          Questions Successfully Submitted
+        </Alert>
+      </Snackbar>
+    </div>
+  );
 }
