194a661619bb0dd86511d1c32f1ed2ae61d6c651
KeshalHetti <kaneeshakeshal@gmail.com>
2023-10-27 17:23:20 +1100

    Added webiste default theme

--- src/Components/tutorschedule.jsx
+++ src/Components/tutorschedule.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { db } from '../config/firebase'; 
+import { db } from '../config/firebase';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -11,6 +11,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import EditIcon from '@mui/icons-material/Edit';
 import Radio from '@mui/material/Radio';
+import BannerBackground from "../Assets/home-banner-background.png";
 
 export default function BasicTable() {
   const [data, setData] = useState([]);
@@ -49,51 +50,65 @@ export default function BasicTable() {
   };
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Timetable</TableCell>
-            <TableCell align="right">Monday</TableCell>
-            <TableCell align="right">Tuesday</TableCell>
-            <TableCell align="right">Wednesday</TableCell>
-            <TableCell align="right">Thursday</TableCell>
-            <TableCell align="right">Friday</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row, rowIndex) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              {['monday', 'tuesday', 'wednesday', 'thursday', 'friday'].map((day, dayIndex) => (
-                <TableCell align="right" key={dayIndex}>
-                  {editIndex === rowIndex && editField === day ? (
-                    initialSubjects.map(subject => (
-                      <div key={subject}>
-                        <Radio
-                          checked={row[day] === subject}
-                          onChange={(e) => handleSubjectChange(e, rowIndex, day)}
-                          value={subject}
-                          name="subject"
-                          inputProps={{ 'aria-label': subject }}
-                        />
-                        {subject}
-                      </div>
-                    ))
-                  ) : (
-                    <>
-                      {row[day]}
-                      <EditIcon onClick={() => handleEdit(rowIndex, day)} style={{ cursor: 'pointer' }} />
-                    </>
-                  )}
-                </TableCell>
-              ))}
+
+    <div className="home-container">
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            BrightBoost - Timetabe
+          </h1>
+        </div>
+      </div>
+
+      <TableContainer className='timetable' component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Timetable</TableCell>
+              <TableCell align="right">Monday</TableCell>
+              <TableCell align="right">Tuesday</TableCell>
+              <TableCell align="right">Wednesday</TableCell>
+              <TableCell align="right">Thursday</TableCell>
+              <TableCell align="right">Friday</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {data.map((row, rowIndex) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                {['monday', 'tuesday', 'wednesday', 'thursday', 'friday'].map((day, dayIndex) => (
+                  <TableCell align="right" key={dayIndex}>
+                    {editIndex === rowIndex && editField === day ? (
+                      initialSubjects.map(subject => (
+                        <div key={subject}>
+                          <Radio
+                            checked={row[day] === subject}
+                            onChange={(e) => handleSubjectChange(e, rowIndex, day)}
+                            value={subject}
+                            name="subject"
+                            inputProps={{ 'aria-label': subject }}
+                          />
+                          {subject}
+                        </div>
+                      ))
+                    ) : (
+                      <>
+                        {row[day]}
+                        <EditIcon onClick={() => handleEdit(rowIndex, day)} style={{ cursor: 'pointer' }} />
+                      </>
+                    )}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
